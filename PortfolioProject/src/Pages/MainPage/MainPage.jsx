d5cfe4e3f1b7883b29f742229bb274147ae3cf80
Henrique Becker <henriquebecker97@hotmail.com>
2023-08-17 20:01:36 -0300

    Update Home

--- PortfolioProject/src/Pages/MainPage/MainPage.jsx
+++ PortfolioProject/src/Pages/MainPage/MainPage.jsx
@@ -1,6 +1,7 @@
 
 import './MainPage.css'
 import Navbar from '../../Components/Navbar/Navbar'
+import myPicture from '../../../public/myPicture.png'
 
 
 function MainPage() {
@@ -9,6 +10,15 @@ function MainPage() {
     <Navbar/>
     <div className='Page'>
       <section className='Section Home'>
+        <section className='CardProfile'>
+          <img src={myPicture} alt="My Picture" />
+          <h2 className='NameHome'>Henrique Becker</h2>
+          <section className='rolesHome'>
+          <p>Quality Assurance Developer</p>
+          <p>Web Developer</p>
+          </section>
+          <section className='ContactsHome'>Contacts</section>
+        </section>
       </section>
       <section className='Section About'></section>
       <section className='Section Skills'></section>
